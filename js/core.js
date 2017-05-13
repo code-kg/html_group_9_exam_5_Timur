fb30e312cd9bdce0792ddb26586a5b2f24ffe5fc
Timurlan <stellfalx@gmail.com>
2017-05-13 18:20:54 +0600

    Working on slider

--- js/core.js
+++ js/core.js
@@ -1,12 +1,24 @@
 $(document).ready(function () {
+
+    $('#menu-btn').click(function(){
+        $('.menu').slideToggle(700);
+        if ($(this).hasClass('not-active')) {
+            $(this).addClass('is-active').removeClass('not-active');
+        }else{
+            setTimeout(function(){
+                $('.is-active').addClass('not-active').removeClass('is-active')
+            },600)
+        }
+    });
+
     var subList = $('.sublist');
     $('.sublist-link').click(function () {
         subList.slideToggle(700).css('background', '#e2534b');
 
     });
     $('.slider').slick({
-        // dots: true,
-        // adaptiveHeight: true
+        dots: true,
+        adaptiveHeight: true
         // autoplay: true,
         // autoplaySpeed: 3000
     });
